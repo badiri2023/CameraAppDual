96dbaaa5fc3dd84f49249f9cc733ffbfbf57b70c
badiri2023 <152429051+badiri2023@users.noreply.github.com>
2026-03-04 19:00:07 +0100

    2.1

--- www/js/index.js
+++ www/js/index.js
@@ -1,25 +1,43 @@
-document.addEventListener('deviceready', onDeviceReady, false);
+document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady() {
-    console.log('Cordova está listo');
-    document.getElementById('btnFoto').addEventListener('click', tomarFoto);
+    document.getElementById("btnTake").addEventListener("click", tomarFoto);
+    document.getElementById("btnRepeat").addEventListener("click", cerrarDialogo);
 }
 
 function tomarFoto() {
     navigator.camera.getPicture(onSuccess, onFail, {
         quality: 50,
-        destinationType: Camera.DestinationType.DATA_URL,
-        sourceType: Camera.PictureSourceType.CAMERA,     
-        correctOrientation: true                         
+        destinationType: Camera.DestinationType.FILE_URI, 
+        sourceType: Camera.PictureSourceType.CAMERA,
+        correctOrientation: true,
+        saveToPhotoAlbum: false
     });
 }
 
-function onSuccess(imageData) {
-    var image = document.getElementById('miImagen');
-    image.style.display = 'block';
-    image.src = "data:image/jpeg;base64," + imageData;
+function onSuccess(imageURI) {
+
+    window.resolveLocalFileSystemURL(imageURI, function(entry) {
+        
+        let imagenMuestra = document.getElementById("previewImage");
+        imagenMuestra.src = entry.toURL(); 
+        
+        // Mostramos la fhoto
+        document.getElementById("dialog").classList.remove("dialog-hidden");
+        
+    }, function(error) {
+        alert("ERRO PROCESING THE PHOTO");
+    });
 }
 
 function onFail(message) {
-    alert('Error o cancelado: ' + message);
+    let msg = message.toLowerCase();
+    if (msg.includes("cancelled") || msg.includes("no image")) {
+        return; 
+    }
+    alert("Camera Error: " + message);
+}
+
+function cerrarDialogo() {
+    document.getElementById("dialog").classList.add("dialog-hidden");
 }
